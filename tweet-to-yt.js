e9d96759aca497340db3d8ffe7a955ad36f9efeb
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-08-23 14:48:40 +0200

    Add notifications and undo

--- tweet-to-yt.js
+++ tweet-to-yt.js
@@ -4,15 +4,33 @@ var YOUTRACK_URL = "https://youtrack.jetbrains.com";
 var ISSUE_ROUTE = YOUTRACK_URL + "/rest/issue";
 var YOUTRACK_PROJECT = "PYMKT";
 
+function basicNotify(message){
+  chrome.notifications.create(null, {
+      "type": "basic",
+      "iconUrl": "youtrack-16.png",
+      "title": "Tweet Reporting",
+      "message": message
+    });
+}
+
+// Apparently event listeners for notifications are a global thing.
+chrome.notifications.onButtonClicked.addListener(handleSuccessNotification);
+function handleSuccessNotification(ticketID, button_index){
+  // The only button is undo, so undo
+  undoReport(ticketID);
+}
+
 function reportTweet(info, tab){
   var linkUrl = info.linkUrl;
   var createTicketRequest = new XMLHttpRequest();
+  var twitter_user;
+  var ticketID;
 
   storageArea.get('token', function(items){
     // Prepare the ticket. We'd like the title to contain the username of the Twitter user
     // this is in the URL: https://twitter.com/<username>/status/<status_id>
     // So the third element of the URL split by '/' should be the username
-    var twitter_user = linkUrl.split("/")[3];
+    twitter_user = linkUrl.split("/")[3];
     var ticket_title = "PyCharm Twitter " + twitter_user;
 
     var ticket_description = linkUrl;
@@ -41,13 +59,54 @@ function reportTweet(info, tab){
 
     // The last segment of the URL is the ticket ID
     // Example: http://unit-258.labs.intellij.net:8080/charisma/rest/issue/TST-1
-    var ticketID = ticketUrl.substr(ticketUrl.lastIndexOf("/") + 1);
+    ticketID = ticketUrl.substr(ticketUrl.lastIndexOf("/") + 1);
 
-    alert('Reported ' + ticketID);
+    var notification = chrome.notifications.create(ticketID, {
+      "type": "basic",
+      "iconUrl": "youtrack-16.png",
+      "title": "Tweet by " + twitter_user + " reported",
+      "message": "Issue ID: " + ticketID,
+      "buttons": [
+        {
+          "title": "Undo"
+        }
+      ]
+    });
+
   }
 
   function onError(e){
-    alert('Error ' + e);
+    basicNotify('Error ' + e);
+  }
+}
+
+function undoReport(issue_id){
+  storageArea.get('token', function(items){
+    // According to the YouTrack API docs, it only accepts the ticket data
+    // as GET style URL parameters. So we'll need to encode these
+    var url = ISSUE_ROUTE + "/" + issue_id;
+
+    var deleteTicketRequest = new XMLHttpRequest();
+
+    // Prepare the DELETE request
+    deleteTicketRequest.addEventListener("load", onLoad);
+    deleteTicketRequest.addEventListener("error", onError);
+    deleteTicketRequest.open("DELETE", url);
+
+    // Add the authentication for the YT REST request
+    var token = items.token;
+    deleteTicketRequest.setRequestHeader("Authorization", "Bearer " + token);
+    deleteTicketRequest.setRequestHeader("Accept", "application/json");
+
+    deleteTicketRequest.send();
+  });
+
+  function onLoad(e){
+    basicNotify("Issue "+ issue_id + " deleted");
+  }
+
+  function onError(e){
+    basicNotify("An error occurred: " + e);
   }
 }
 
